4c9ace43793e66bd19383f3291ff59c63ecdf802
stefanpupo4 <stefanpupovic345@gmail.com>
2026-06-10 16:50:31 +0200

    Update HP_Stamina.py

--- HP_Stamina.py
+++ HP_Stamina.py
@@ -1,36 +1,31 @@
 import pygame
 
-MAX_HEALTH=100
-MAX_STAMINA=100
-MAGIC_EFFECTS_REQUIRED=4
-STAMINA_FILL_TIME=4000
+MAX_HEALTH = 100
+MAX_STAMINA = 100
+MAGIC_EFFECTS_REQUIRED = 4
+STAMINA_FILL_TIME = 4000
 
-HEALTH_BAR=400
-HEALTH_BAR_HEIGHT=30
-STAMINA_BAR_HEIGHT=10
+HEALTH_BAR = 400
+HEALTH_BAR_HEIGHT = 30
+STAMINA_BAR_HEIGHT = 10
 def setup_stats(fighter):
-  fighter.health=MAX_HEALTH
-  fighter.magic_effects=0
-  fighter.run_stamina=20
-  figher.stamina_fill_multiplier=1
-  fighter.last_stamina_update=
-pygame.time.get_ticks()
+  fighter.health = MAX_HEALTH
+  fighter.magic_effects = 0
+  fighter.run_stamina = 20
+  fighter.stamina_fill_multiplier = 1
+  fighter.last_stamina_update=pygame.time.get_ticks()
 
 def update_stamina(fighter):
-  current_time=pygame.time.get_ticks()
-  elapsed_time=current_time-
-fighter.last_stamina_update
-  fighter.last_stamina_update=
-current_time
+  current_time = pygame.time.get_ticks()
+  elapsed_time = current_time-fighter.last_stamina_update
+  fighter.last_stamina_update = current_time
 
-  if fighter.run_stamina<MAX_STAMINA:
-     fighter.run_stamina +=MAX_STAMINA
-* elapsed_time *
-fighter.stamina_fill_multiplier /
-STAMINA_FILL_TIME
-     if fighter.run_stamina>
-MAX_STAMINA:
-      fighter.run_stamina>
-MAX_STAMINA:
+  if fighter.run_stamina < MAX_STAMINA:
+     fighter.run_stamina += MAX_STAMINA * elapsed_time * fighter.stamina_fill_multiplier / STAMINA_FILL_TIME 
+     if fighter.run_stamina > MAX_STAMINA:
+      fighter.run_stamina = MAX_STAMINA:
 else:
-      fighter.stamina_fill_multiplier=1
+      fighter.stamina_fill_multiplier = 1
+
+def draw_status_bars(screen,fighter,x,y):
+  health_ratio = fighter.health / MAX_HEALTH
