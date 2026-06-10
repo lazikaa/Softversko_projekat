81dc96d8955d256ccbd3008c7a6d805d4d018c16
lazikaa <lazdasgit@gmail.com>
2026-06-10 15:55:04 +0200

    Update borci.py

--- borci.py
+++ borci.py
@@ -3,9 +3,9 @@ import random
 import pygame
 from Sound import zvuk_udarac, zvuk_skok, zvuk_run1, zvuk_run1, zvuk_run2, zvuk_miss
 from HP_Stamina import (
-MAGIC_EFFECTS_REQUIRED,
-MAX_HEALTH, 
-MAX_STAMINA,
-setup_stats,
-update_stamina,
+  MAGIC_EFFECTS_REQUIRED,
+  MAX_HEALTH, 
+  MAX_STAMINA,
+  setup_stats,
+  update_stamina,
 )
