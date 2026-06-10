35723d60d42fb98a9a4afbb66c753cfb21a332cb
ognjen3749 <ognjen3749@gmail.com>
2026-06-10 12:39:23 +0200

    Create ResourcePath.py

---
+++ ResourcePath.py
@@ -0,0 +1,9 @@
+from pathib import path
+import sys
+
+def resoursce_path(*parts):
+  if hasattr(sys, "_MEIPASS")
+  base_dir = Path(sys._MEIPASS)
+  else:
+    base_dir = Path(__file__).resolve().parent
+    return str(base_dir.joinpath((*paths))
