481f4812b85b6de286e7615594f2fa82e0138973
vukbrnovic <vukbrnovic9@gmail.com>
2026-06-10 23:03:44 +0200

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -6,4 +6,19 @@ from VolumeSlider import draw_volume_slider, volume_from_mouse
 
 CHARACTER_PREVIEWS = [
     {
-        "id": "woody"
+        "id": "woody",
+        "name": "Woody",
+        "sheet": ("CHARACTERS", "Woody.png"),
+        "size": 80,
+        "scale": 1.7,
+        "offset": (38, -19),
+    },
+    {
+         "id": "davies",
+         "name": "Davies",
+         "sheet": ("CHARACTERS", "Davies.png"),
+         "size": 80,
+         "scale": 1.7,
+         "offset": (38, -19),
+    },
+]
