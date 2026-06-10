8af4c735f55436c3ab8558f904376f803504cb6e
vukbrnovic <vukbrnovic9@gmail.com>
2026-06-10 22:55:08 +0200

    Create Menu.py

---
+++ Menu.py
@@ -0,0 +1,9 @@
+import pygame
+import sys
+from ResourcePath import resource_path
+from Sound import podesi_ui_volumen, pusti_muziku_menija, zvuk_click
+from VolumeSlider import draw_volume_slider, volume_from_mouse
+
+CHARACTER_PREVIEWS = [
+    {
+        "id": "woody"
