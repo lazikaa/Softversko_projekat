a14ffd0343ba832fa3c6e411c4e301932a833085
vukbrnovic <vukbrnovic9@gmail.com>
2026-06-10 23:08:22 +0200

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -22,3 +22,18 @@ CHARACTER_PREVIEWS = [
          "offset": (38, -19),
     },
 ]
+
+BOJE = {
+    "bijela": (255, 255, 255),
+    "crvena": (255, 0, 0),
+    "zuta": (255, 255, 0),
+    "siva": (100, 100, 100),
+    "crna": (0, 0, 0)
+}
+
+
+def nacrtaj_tekst_centrirano(screen, tekst, font, boja, y, SCREEN_WIDTH):
+    img = font.render(tekst, True, boja)
+    rect = img.get_rect(center=(SCREEN_WIDTH // 2, y))
+    screen.blit(img, rect)
+    return rect
