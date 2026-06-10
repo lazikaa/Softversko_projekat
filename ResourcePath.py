b6eb04b82e9d5832cdbc0eef1d16cd62cbbcfcc8
ognjen3749 <ognjen3749@gmail.com>
2026-06-10 22:44:59 +0200

    Update ResourcePath.py

--- ResourcePath.py
+++ ResourcePath.py
@@ -1,9 +1,9 @@
-from pathib import path
+from pathlib import Path
 import sys
 
 def resoursce_path(*parts):
-  if hasattr(sys, "_MEIPASS")
-  base_dir = Path(sys._MEIPASS)
-  else:
-    base_dir = Path(__file__).resolve().parent
+    if hasattr(sys, "_MEIPASS"):
+        base_dir = Path(sys._MEIPASS)
+    else:
+        base_dir = Path(__file__).resolve().parent
     return str(base_dir.joinpath((*paths))
