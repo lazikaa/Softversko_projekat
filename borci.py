6a8299090e39f07f291c5a95ec74b0f4b7463750
lazikaa <lazdasgit@gmail.com>
2026-06-12 19:09:01 +0200

    Update borci.py

--- borci.py
+++ borci.py
@@ -324,6 +324,7 @@ class Fighter():
                     # Ako je igrac u sred napada, napad se zaustavlja
                         self.attacking = False
                         self.attack_cooldown = 20
+                        
     def attack(self, surface, target):
         if self.attack_cooldown == 0 and self.hit == False:
             self.attacking = True
