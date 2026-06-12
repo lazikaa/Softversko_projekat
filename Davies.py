c7fad1b82caba4d0320bed8bed9cbe7c28c6ea84
stefanpupo4 <stefanpupovic345@gmail.com>
2026-06-12 11:52:54 +0200

    Davies.py

---
+++ Davies.py
@@ -0,0 +1,16 @@
+import pygame
+from borci import Fighter
+from HP_Stamina import MAGIC_EFFECTS_REQUIRED
+from ResourcePath import resource_path
+from Sound import zvuk_udarac
+
+DAVIES_SHEET = pygame.image.load(resource_path("CHARACTERS", "DAVIES.png"))
+DAVIES_DATA = [80, 1.7, [38, -19]]
+DAVIES_ANIMATION = [8, 4, 5, 5, 8, 8, 3, 8, 8]
+DAVIES_LABEL = "Davies"
+DAVIES_ROUND_WIN_TEXT = "Davies WINS THE ROUND!"
+
+class Davies(fighter):
+  def use_special(self):
+    if not self.special and not self.attacking and not self.hit and not self.knockdown and self.alive and self.magic_effects >=MAGIC_EFFECTS_REQUIRED:
+      self
