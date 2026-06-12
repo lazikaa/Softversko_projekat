c5a39d134688366687514767ee20e45449ac7117
stefanpupo4 <stefanpupovic345@gmail.com>
2026-06-12 11:40:04 +0200

    Update HP_Stamina.py

--- HP_Stamina.py
+++ HP_Stamina.py
@@ -30,3 +30,21 @@ def update_stamina(fighter):
 
 def draw_status_bars(screen,fighter,x,y):
   health_ratio = fighter.health / MAX_HEALTH
+  magic_ratio = fighter.magic_effects / MAGIC_EFFECTS_REQUIRED
+  run_ratio = fighter.run_stamina / MAX_STAMINA
+  
+  pygame.draw.rect(screen, (255,255,255), (x - 2 y - 2, HEALTH_BAR_WIDTH +4, HEALTH_BAR_HEIGHT + STAMINA_BAR_HEIGHT + 4))
+  pygame.draw.rect(screen, (255,0,0), (x, y, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
+    pygame.draw.rect(screen, (150,80,255), (x, y, HEALTH_BAR_WIDTH * health_ratio, HEALTH_BAR_HEIGHT))
+stamina_y - y + HEALTH_BAR_HEIGHT
+half_width - HEALTH_BAR_WIDTH // 2
+pygame.draw.rect(screen, (35,25,60), (x, stamina_y, half_width; STAMINA_BAR_HEIGHT))
+pygame.draw.rect(screen, (20,60,20), (x + half_width, stamina_y, half_width, STAMINA_BAR_HEIGHT)
+pygame.draw.rect(screen, (150,80,255), (x, stamina_y, half_width * magic_ratio, STAMINA_BAR_HEIGHT)
+pygame.draw.rect(screen, (0,220,0), (x + half_width, stamina_y, half_width * run_ratio,STAMINA_BAR_HEIGHT)
+pygame.draw.rect(screen, (255,255,255), (x + half_width, stamina_y), (x + half_width, stamina_y + STAMINA-BAR_HEIGHT),1)
+                 
+for i in range(1, MAGIC_EFFECTS_REQUIRED):
+   segment_x - x + i * half_width // MAGIC_EFFECTS_REQUIRED
+    pygame.draw.line(screen, (255,255,255), (segment_x, stamina_y), (segment_x, stamina_y + STAMINA_BAR_HEIGHT),1)
+
