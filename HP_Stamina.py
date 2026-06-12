74d39f55cec5e3a673090e68654adb4163f8cdf2
lazikaa <lazdasgit@gmail.com>
2026-06-12 22:08:02 +0200

    Fix indentation in setup_stats function

--- HP_Stamina.py
+++ HP_Stamina.py
@@ -10,11 +10,11 @@ HEALTH_BAR_HEIGHT = 30
 STAMINA_BAR_HEIGHT = 10
 
 def setup_stats(fighter):
-  fighter.health = MAX_HEALTH
-  fighter.magic_effects = 0
-  fighter.run_stamina = 20
-  fighter.stamina_fill_multiplier = 1
-  fighter.last_stamina_update=pygame.time.get_ticks()
+		fighter.health = MAX_HEALTH
+  	fighter.magic_effects = 0
+  	fighter.run_stamina = 20
+  	fighter.stamina_fill_multiplier = 1
+  	fighter.last_stamina_update=pygame.time.get_ticks()
 
 def update_stamina(fighter):
   current_time = pygame.time.get_ticks()
