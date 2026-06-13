58b17bd8f7c3b3043e0921d14fffa261351d860c
miilosc <cuparamilos11@gmail.com>
2026-06-13 19:56:59 +0200

    Update UI.py

--- UI.py
+++ UI.py
@@ -27,3 +27,60 @@ def napravi_overlay(screen, alpha, sirina, visina):
   overlay.fill(BOJE["crna"])
   screen.blit(overlay, (0, 0))
   
+# ELEMENTI IGRE
+
+def draw_tally_score(screen, score, x, y, font):
+  # Score label
+  lbl_rect = nacrtaj_tekst(screen, "Score: ", font, BOJE["zuta"], x, y)
+  curr_x = x + lbl_rect.width + 5
+
+# Grupe po 5 i ostatak
+za_crtanje = [(score // 5, True), (score % 5, False)]
+
+for kolicina, kosa_crta in za_crtanje:
+  for _ in range(kolicina):
+    if kosa_crta:
+      nacrtaj_tekst(screen, "IIII", font, BOJE["bijela"], curr_x, y)
+      pygame.draw.line(screen, BOJE["bijela"], (curr_x, y + 25), (curr_x + 35, y + 5), 3)
+      curr_x += 50
+    else:
+      txt = "I" * kolicina
+      nacrtaj_tekst(screen, txt, font, BOJE["bijela"], curr_x, y)
+      break
+
+def draw_victory_message(screen, pobjednik, font, sirina, visina):
+  nacrtaj_tekst(screen, pobjednik, font, BOJE["zuta"], sirina // 2, visina // 2 - 50, True)
+
+# MENIJI
+
+def potvrda_izlaska(screen, clock, sirina, visina):
+  font_q = pygame.font.SysFont("Constantia", 40, bold=True)
+  font_btn = pygame.font.Sys.Font("Constantia", 35)
+
+  while True:
+    napravi_overlay(screen, 220, sirina, visina)
+    mx, my = pygame.mouse.get_pos()
+
+    nacrtaj_tekst(screen, "Are You Sure You Want To Quit?", font_q, BOJE["bijela"], sirina // 2, 250, True)
+
+    y_rect = pygame.Rect(sirina // 2 - 100, 320, 80, 40)
+    n_rect = pygame.Rect(sirina // 2 + 30, 320, 80, 40)
+
+    # YES / NO dugmad
+    for rect, txt, action in [(y_rect, "YES", True), (n_rect, "NO", False)]:
+      boja = BOJE["crvena"] if rect.collidepoint((mx, my)) else BOJE["bijela"]
+      nacrtaj_tekst(screen, tekst, font_btn, boja, rect.x, rect.y)
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT: pygame.quit(); sys.exit()
+      if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+          if y_rect.collidepoint((mx, my)):
+            zvuk_click()
+            pygame.quit(); sys.exit()
+          if n_rect.collidepoint((mx, my)):
+            zvuk_click()
+            return
+
+    pygame.display.update()
+    clock.tick(60)
+    
