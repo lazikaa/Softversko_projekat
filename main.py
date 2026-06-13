be836726974582a9182b7d1c59c4b288673226b6
lazikaa <lazdasgit@gmail.com>
2026-06-13 12:18:23 +0200

    Update main.py
    Povezivanje fajlova

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+import pygame
+import sys
+from Davies import DAVIES_LABEL, DAVIES_ROUND_WIN_TEXT, create_davies
+from Woody import WOODY_LABEL, WOODY_ROUND_WIN_TEXT, create_woody
+from Menu import pokreni_meni
+from Sound import pusti_muziku_menija, pusti_nasumicnu_muziku_borbe, zaustavi_muziku_borbe
+from UI import draw_tally_score, draw_victory_message, prikazi_pauzu
+from HP_Stamina import draw_status_bars
+from ResourcePath import resource_path
