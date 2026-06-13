6ece99d8b8b9f5626dd4305cdab5853f83097c96
lazikaa <lazdasgit@gmail.com>
2026-06-13 13:07:39 +0200

    Update main.py
    Kontrole podesavanja resursi

--- main.py
+++ main.py
@@ -7,3 +7,40 @@ from Sound import pusti_muziku_menija, pusti_nasumicnu_muziku_borbe, zaustavi_mu
 from UI import draw_tally_score, draw_victory_message, prikazi_pauzu
 from HP_Stamina import draw_status_bars
 from ResourcePath import resource_path
+
+# Kontrole
+player_1_controls = {
+    "left": pygame.K_a,
+    "right": pygame.K_d,
+    "jump": pygame.K_w,
+    "run": pygame.K_s,
+    "attack1": pygame.K_t,
+    "attack2": pygame.K_z,
+    "special": pygame.K_u,
+}
+player_2_controls = {
+    "left": pygame.K_LEFT,
+    "right": pygame.K_RIGHT,
+    "jump": pygame.K_UP,
+    "run": pygame.K_DOWN,
+    "attack1": pygame.K_KP1,
+    "attack2": pygame.K_KP2,
+    "special": pygame.K_KP3,
+}
+
+CHARACTERS = {
+    "woody": {
+        "create": create_woody,
+        "label": WOODY_LABEL,
+        "round_win_text": WOODY_ROUND_WIN_TEXT,
+    },
+    "davies": {
+        "create": create_davies,
+        "label": DAVIES_LABEL,
+        "round_win_text": DAVIES_ROUND_WIN_TEXT,
+    },
+}
+
+# Resursi
+bg_image = pygame.image.load(resource_path("Pozadine", "W3.jpg"))
+
